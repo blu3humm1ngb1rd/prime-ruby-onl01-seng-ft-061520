6603bcb095e94050907433278256fc681bae90ca
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 16:00:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,28 +3,7 @@
 
 def prime?(number)
   
-  number.each do |num|
-    if num == 1 
-       false 
-      elsif num == 2 || 3 
-       true
-    elsif num % 2 == 0 
-     false
-    elsif  num % 3 == 0
-     false
-    elsif num % 5 == 0
-     false
-    elsif num % 7 == 0
-     false
-    elsif num % 9 == 0
-     false
-    elsif num % 11 == 0
-   false
-    
-  else 
-    return true 
-  end
-end
+(1..number).to_a 
   
   
 end
