1fdcdc03f3b76d7e6218615fd0f622694c495881
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 16:36:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,12 @@ def prime?(number)
    false 
 else 
   (2..number).to_a.each do |num|
-    num % number 
+   #  num % number  #not prime is false
+   # test number using range number if divisible by any in range; then not prime
+   # 7 -> 2,3,4,5,6,7 
+   if num % number == 0 
+     false 
+   
 end
   
   
