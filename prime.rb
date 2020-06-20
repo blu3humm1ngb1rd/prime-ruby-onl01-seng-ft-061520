08dd40a73f81b6b7f386e438c7d583c41f50bdf0
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 16:46:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -12,9 +12,10 @@ else
    # test number using range number if divisible by any in range; then not prime
    # 7 -> 2,3,4,5,6
    #8 -> 2, 3, 4,5,6,7
-   number % num == 0 
+   new_list = []
+new_list << number % num == 0 
     
-   #new_list = []
+   new_list.none?(0) 
 end
   
   
