30cd96e11619ab69b4fd4448b8a60bed2c4f48a8
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 16:44:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,12 +7,12 @@ def prime?(number)
  if number < 2
    false 
 else 
-  (2..number-1).to_a. do |num|
+  (2..number-1).to_a.each do |num|
    #  num % number  #not prime is false
    # test number using range number if divisible by any in range; then not prime
    # 7 -> 2,3,4,5,6
    #8 -> 2, 3, 4,5,6,7
-   num % number == 0 
+   number % num == 0 
     
    #new_list = []
 end
